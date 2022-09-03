f18bc8a4ffd259a8b15aa7bda9392049dc47148c
jayson <jays.labarrete@gmail.com>
2022-09-03 23:16:31 +1000

    update tests

--- uiTests/uitests/createRecord.spec.ts
+++ uiTests/uitests/createRecord.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
+import { matchers } from 'playwright-expect';
 import { User } from './data/user';
 import { AddCustomerPage } from './pages/addcustomer.page';
 import { EditCustomerPage } from './pages/editCustomer.page';
 import { LoginPage } from './pages/login.page';
 
 let testUser :User
+
+// add custom matchers
+// expect.extend(matchers);
 
 test.beforeAll(() => {
     testUser = new User().generateRandomUser();
@@ -24,7 +28,7 @@ test('Happy Path - access and update consumer details', async ({ page }) => {
   
 })
 
-test.only('Negative Path - remove customer data and attempt to save', async ({page}) => {
+test('Negative Path - remove customer data and attempt to save', async ({page}) => {
   const loginPage = new LoginPage(page);
   await loginPage.goTo()
   await loginPage.login()
@@ -34,9 +38,12 @@ test.only('Negative Path - remove customer data and attempt to save', async ({pa
 
   const editCustomerPage = new EditCustomerPage(page);
   await editCustomerPage.updateCustomerFieldsBlank()
-  await page.pause()
-  await expect ((await page.locator('text=Address Field must not be blank')).tobeVisible())
-    
+  await expect(await page.locator('text=Address Field must not be blank')).toBeVisible()
+  await expect(await page.locator('text=City Field must not be blank')).toBeVisible()
+  await expect(await page.locator('text=State must not be blank')).toBeVisible()
+  await expect(await page.locator('text=PIN Code must not be blank')).toBeVisible()
+  await expect(await page.locator('text=Mobile no must not be blank')).toBeVisible()
+  await expect(await page.locator('text=Email-ID must not be blank')).toBeVisible()  
 })
 
 test('Negative Path - update customer data with special characters', async ({page}) => {
@@ -49,10 +56,10 @@ test('Negative Path - update customer data with special characters', async ({pag
 
   const editCustomerPage = new EditCustomerPage(page);
   await editCustomerPage.updateCustomerFieldsSpecialChar()
-  
+  await expect(await page.locator('text=Special characters are not allowed').count()).toEqual(5)
 })
 
-test ('Negative Path - update customer data with numbers', async ({page}) => {
+test('Negative Path - update customer data with numbers', async ({page}) => {
   const loginPage = new LoginPage(page);
   await loginPage.goTo()
   await loginPage.login()
@@ -62,7 +69,7 @@ test ('Negative Path - update customer data with numbers', async ({page}) => {
 
   const editCustomerPage = new EditCustomerPage(page);
   await editCustomerPage.updateCustomerFieldsNumber(testUser)
-
+  await expect(await page.locator('text=Numbers are not allowed').count()).toEqual(2)
 })
 
 test('Negative Path - update customer data with emoji', async ({page}) => {
@@ -75,5 +82,6 @@ test('Negative Path - update customer data with emoji', async ({page}) => {
 
   const editCustomerPage = new EditCustomerPage(page);
   await editCustomerPage.updateCustomerFieldsEmoji()
-  await page.pause()
+  await expect(await page.locator('text=Numbers are not allowed').count()).toEqual(2)
+  await expect(await page.locator('text=Characters are not allowed').count()).toEqual(2)
 })
